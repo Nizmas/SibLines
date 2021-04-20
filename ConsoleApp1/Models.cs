419a28518b8907a35912325077e1722f65681ae1
Nizmas <89174507049@yandex.ru>
2021-04-20 21:26:53 +0500

    logic out of main

--- ConsoleApp1/Models.cs
+++ ConsoleApp1/Models.cs
@@ -1,6 +1,6 @@
-
-
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ConsoleApp1
 {
@@ -8,5 +8,26 @@ namespace ConsoleApp1
     {
         public int x { get; set; }
         public int y { get; set; }
+    }
+    
+    public class NSegments
+    {
+        public List<List<NPoint>>  linesList { get; set; }
+        public List<List<NPoint>> rectangleList { get; set; }
+
+        public NSegments(string linesName, string rectangleName)
+        {
+            string lineJson, rectangleJson;
+            FileStream lineStream = new FileStream($"{linesName}.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(lineStream))
+            { lineJson = reader.ReadLine(); }
+            
+            FileStream rectangleStream = new FileStream($"{rectangleName}.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(rectangleStream))
+            { rectangleJson = reader.ReadLine(); }
+            
+            linesList = JsonConvert.DeserializeObject<List<List<NPoint>>>(lineJson);
+            rectangleList = JsonConvert.DeserializeObject<List<List<NPoint>>>(rectangleJson);
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace ConsoleApp1
 {
@@ -14,23 +11,9 @@ namespace ConsoleApp1
         {
             int width = 700;
             int height = 700;
-            var lines = "Lines";
-            var rectangle = "Rectangle";
-            string lineJson, rectangleJson;
-            string baseDir = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("ConsoleApp1"));
-            Console.WriteLine(baseDir);
 
-            FileStream lineStream = new FileStream($"{baseDir}Points/{lines}.txt", FileMode.Open);
-            using (StreamReader reader = new StreamReader(lineStream))
-            { lineJson = reader.ReadLine(); }
-            
-            FileStream rectangleStream = new FileStream($"{baseDir}Points/{rectangle}.txt", FileMode.Open);
-            using (StreamReader reader = new StreamReader(rectangleStream))
-            { rectangleJson = reader.ReadLine(); }
-            
-            var linesList = JsonConvert.DeserializeObject<List<List<NPoint>>>(lineJson);
-            var rectangleList = JsonConvert.DeserializeObject<List<List<NPoint>>>(rectangleJson);
-            
+            var segments = new NSegments("Lines", "Rectangle");
+
             using(var image = new Bitmap(width, height))
             {
                 using (var graphics = Graphics.FromImage(image))
@@ -44,23 +27,23 @@ namespace ConsoleApp1
                     graphics.CompositingMode = CompositingMode.SourceCopy;
                     graphics.FillRegion(whiteBrush, fillRegion);
                     
-                    rectangleList.ForEach(delegate (List<NPoint> line)
+                    segments.rectangleList.ForEach(delegate (List<NPoint> line)
                     {
                         Pen currentPen = new Pen(Brushes.DeepSkyBlue);
                         graphics.DrawLine(currentPen, line[0].x, line[0].y, line[1].x, line[1].y);
                     });
                     
-                    linesList.ForEach(delegate (List<NPoint> line)
+                    segments.linesList.ForEach(delegate (List<NPoint> line)
                     {
                         Pen currentPen = new Pen(Brushes.Black);
-                        var iS = new NIntersection(rectangleList, line);
+                        var iS = new NIntersection(segments.rectangleList, line);
                         if (iS.CheckIntersec())
                             currentPen = new Pen(Brushes.Red);
                         
                         graphics.DrawLine(currentPen, line[0].x, line[0].y, line[1].x, line[1].y);
                     });
                     
-                    image.Save($"{baseDir}Painted {lines}.png", ImageFormat.Png);
+                    image.Save("Painted lines.png", ImageFormat.Png);
                 }       
             } 
         }
